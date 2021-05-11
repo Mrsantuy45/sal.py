693112e04c141ae72e4f91670eb878f2d26b01d8
iswantohacker <84025897+Mrsantuy45@users.noreply.github.com>
2021-05-11 21:31:17 +0700

    Add files via upload

---
+++ Termux.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+clear
+
+toilet -f big -F gay WELCOM  TO	 TERMU
+#This colour
+pink='\033[1;35m'
+cyan='\033[36;1m'
+green='\033[34;1m'
+okegreen='\033[32;1m'
+lightgreen='\033[32;1m'
+white='\033[37;1m'
+red='\033[31;1m'
+yellow='\033[33;1m'
+BlueF='\033[34;1m'
+sleep 1
+echo
+toilet -f big -F gay KONTOL
+sleep 1
+echo
+echo $cyan"silahkan pilih tools nya:"
+echo
+echo $yellow"1.)stabilkan jaringan"
+echo
+echo $red"0.) keluar aja!!!"
+read bad
+
+if [ $bad = 1 ] || [ $bad = 1 ]
+then
+clear
+echo $red
+toilet -f big -F gay asu!!!
+sleep 2
+echo $BlueF"setabilkan jaringan"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bad  = 0 ] || [ $bad = 0 ]
+then
+echo $red
+figlet bye
+echo $BlueF
+echo "tunggu 3 detik untuk keluar"
+sleep 3
+exit
+fi
+
